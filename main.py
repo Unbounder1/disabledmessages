dc4151ca378ad1f7e008d47f28b5fe5a80ab6339
Unbounder1 <78339287+Unbounder1@users.noreply.github.com>
2022-07-04 12:04:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import re
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+@client.event
+async def on_message(message):
+    if message.author.bot: return
+    cantuse = open('forbiddenmessages', 'r').read().splitlines()
+    for i in cantuse:
+        if re.match(i, message.content, re.I) == None:
+            continue
+        else:
+            await message.delete()
+            await message.channel.send('DO NOT USE THAT LANGUAGE HERE')
+            print (str(message.author) + " used " + str(i))
+            return
+        return
+with open('config', "r") as config:
+
+    clientid = config.readlines()
+
+client.run(clientid[1])
